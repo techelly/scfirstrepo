9853c2122154e2112f79057fa8b59331c79258f8
prabhat1510 <prabhatc@gmail.com>
2023-08-09 10:59:40 +0530

    Advanced concept example

--- customerapp/src/components/customers/CustomerList.js
+++ customerapp/src/components/customers/CustomerList.js
@@ -1,7 +1,12 @@
+import customers from '../../customers.json';
 function CustomerList(){
+    const numbers=[1,2,3,4,5];//arrays of numbers
+    const listItems = numbers.map((number)=><li>{number}</li>);
     return(
         <>
             <h1>Customers List</h1>
+            <ul>{listItems}</ul>
+            <p>{JSON.stringify(customers)}</p>
         </>
     );
 }
