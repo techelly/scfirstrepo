5901b0793fe9259116c78b4670adad5b36f35355
prabhat1510 <prabhatc@gmail.com>
2023-08-23 17:24:46 +0530

    Fixed Swagger issues

--- usecasetwousingspring/src/main/java/com/scb/usecasetwousingspring/UsecasetwousingspringApplication.java
+++ usecasetwousingspring/src/main/java/com/scb/usecasetwousingspring/UsecasetwousingspringApplication.java
@@ -2,6 +2,11 @@ package com.scb.usecasetwousingspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 
 @SpringBootApplication
 public class UsecasetwousingspringApplication {
@@ -9,5 +14,7 @@ public class UsecasetwousingspringApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UsecasetwousingspringApplication.class, args);
 	}
+	
+	
 
 }
